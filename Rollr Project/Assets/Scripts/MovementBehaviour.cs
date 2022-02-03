4af3580359dfde6340acfb0824434a77287e06e9
Darick Cochrane <boomrocks24@gmail.com>
2022-02-03 00:14:47 -0700

    Added movement for camera and background

---
+++ Rollr Project/Assets/Scripts/MovementBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour
+{
+    public float speed;
+    public bool canMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove == true)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed,Space.World);  
+        }
+    }
+}
