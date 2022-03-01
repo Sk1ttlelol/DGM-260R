ce7fe718f0b66e90a4bf3c7348cf9b3321c5f0ff
Darick <SlamminD2@hotmail.com>
2022-03-01 14:56:00 -0700

    Collectible system revamped and cleaned. Allows for shop with SO int data

--- Rollr Project/Assets/Scripts/TextUpdatebehvaiour.cs
+++ Rollr Project/Assets/Scripts/TextUpdatebehvaiour.cs
@@ -2,21 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class TextUpdatebehvaiour : MonoBehaviour
 {
    public Text CollectibleAmount;
    public IntData CollectibleIntValue;
+
+   public UnityEvent textUpdateEvent;
    private int coins;
 
    public void Start()
    {
-      CollectibleIntValue.value = coins;
+      //CollectibleIntValue.value = coins;
+   }
+
+   private void Update()
+   {
+      textUpdateEvent.Invoke();
    }
 
    public void UpdateTextValue(IntData obj)
    {
-      CollectibleAmount.text = obj.value.ToString("0");
+      CollectibleAmount.text = obj.value.ToString();
    }
 }
