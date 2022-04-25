3d8993eabada2fa83213e03752d4e46fc70d0f7b
Darick Cochrane <boomrocks24@gmail.com>
2022-04-25 15:50:22 -0600

    First pass at publish

--- Rollr Project/Assets/Scripts/FlippingPlatformsBehaviour.cs
+++ Rollr Project/Assets/Scripts/FlippingPlatformsBehaviour.cs
@@ -8,17 +8,8 @@ public class FlippingPlatformsBehaviour : MonoBehaviour
     public float repeatFrequency = 2.5f;
 
     public bool flipPlatform = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
     
     public void StartFlip()
     {
